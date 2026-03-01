77d7862a33b55b92883b3403bd93461ed678eddc
Howard <hw@yuanhau.com>
2026-03-01 14:03:01 +0800

    Add stuff

---
+++ code/dont-use-client-side/break.ts
@@ -0,0 +1,23 @@
+/*
+if (checkpass.substring(0, 4) == 'pico') {
+      if (checkpass.substring(24, 28) == 'eb02') {
+        if (checkpass.substring(4,8) == 'CTF{') {
+         if (checkpass.substring(16, 20) == 'ts_p') {
+          if (checkpass.substring(12, 16) == 'lien') {
+            if (checkpass.substring(20, 24) == 'lz_2') {
+              if (checkpass.substring(8, 12) == 'no_c') {
+                if (checkpass.substring(28, 32) == 'b45}') {
+                  alert("Password Verified")
+                  }
+                }
+              }
+
+            }
+          }
+        }
+      }
+    }*/
+
+const split = 4;
+
+//picoCTF{no_clients_plz_2eb02b45}
